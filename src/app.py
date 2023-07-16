c69a1a7db76cdcb234b55ddf0429c5d5ffff7114
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-16 21:04:18 +0530

    detect if a rebuild is required

--- src/app.py
+++ src/app.py
@@ -11,6 +11,11 @@ def trigger_build(webhook_name: str):
     if webhook_name not in CONFIG.keys():
         return "No such repository configured.", 404
 
+    repo_path = REPO_DIR / webhook_name
+    if not repo_has_changes(repo_path):
+        return "No changes detected. Go for a walk, come back, make some changes, push :)", 420
+    pull(repo_path)
+
     return "Done"
 
 
--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,6 @@
 import tomllib
 from pathlib import Path
-from subprocess import run
+from subprocess import run, check_output
 
 APP_DIR = Path(__file__).parent.parent
 CONFIG_DIR = APP_DIR / "config/"
@@ -18,3 +18,15 @@ def clone_repos():
 
 def delete_repos():
     run(["rm", "-rf", REPO_DIR])
+
+
+def repo_has_changes(repo_path: Path | str):
+    run(["git", "remote", "update"], cwd=repo_path)
+    branch = check_output(["git", "branch", "--show-current"]).decode().strip()
+    remote = check_output(["git", "rev-parse", f"origin/{branch}"], cwd=repo_path).strip()
+    local = check_output(["git", "rev-parse", branch], cwd=repo_path).strip()
+    return remote != local
+
+
+def pull(repo_path: Path | str):
+    run(["git", "pull"], cwd=repo_path)
