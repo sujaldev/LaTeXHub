7daad2d2cd0238681cf1e629b9427ba6bd00d776
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-16 20:40:42 +0530

    init webhook

--- src/app.py
+++ src/app.py
@@ -6,6 +6,14 @@ clone_repos()
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, world!"
+@app.route("/webhook/<webhook_name>")
+def trigger_build(webhook_name: str):
+    if webhook_name not in CONFIG.keys():
+        return "No such repository configured.", 404
+
+    return "Done"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 54321)
+    delete_repos()
