62209b35f890c92248fff280abd8e5d53e7c01ac
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-17 19:30:34 +0530

    integrate everything in main app

--- src/app.py
+++ src/app.py
@@ -1,24 +1,26 @@
-from helpers import *
+from build import Builder
+from git_helpers import rebuild_is_required
+from env import is_authorized_user, verify_signature
 
-from flask import Flask
+from flask import Flask, request
 
-clone_repos()
 app = Flask(__name__)
 
 
-@app.route("/webhook/<webhook_name>")
-def trigger_build(webhook_name: str):
-    if webhook_name not in CONFIG.keys():
-        return "No such repository configured.", 404
+@app.route("/webhook/<user>/<repo>")
+def trigger_build(user: str, repo: str):
+    if not verify_signature(request):
+        return "Incorrect signature.", 403
 
-    repo_path = REPO_DIR / webhook_name
-    if not repo_has_changes(repo_path):
-        return "No changes detected. Go for a walk, come back, make some changes, push :)", 420
-    pull(repo_path)
+    if not is_authorized_user(user):
+        return "This user/organization is not permitted to trigger a build.", 403
 
-    return "Done"
+    if not rebuild_is_required(user, repo):
+        return "No changes detected, rebuild not required."
+
+    Builder(user, repo).build()
+    return "Done!"
 
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 54321)
-    delete_repos()
