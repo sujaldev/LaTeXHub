d2ce45f456fb34f47417f0e8ec051ec31ffea428
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-17 19:27:53 +0530

    add authorization helpers

---
+++ src/env.py
@@ -0,0 +1,26 @@
+import os
+import hmac
+import hashlib
+
+import flask
+
+ALLOWED_USERS = os.getenv("ALLOWED_USERS", "*").split(",")
+SECRET = os.getenv("SECRET")
+
+assert not (SECRET is None or SECRET == "REPLACE_WITH_YOUR_SECRET_KEY")
+
+
+def is_authorized_user(user: str) -> bool:
+    return user in ALLOWED_USERS or "*" in ALLOWED_USERS
+
+
+def verify_signature(request: flask.Request) -> bool:
+    # As described here:
+    # https://docs.github.com/en/webhooks-and-events/webhooks/securing-your-webhooks#python-example
+
+    if signature_header := request.headers.get("X-Hub-Signature-256"):
+        return False
+
+    hash_object = hmac.new(SECRET.encode('utf-8'), msg=request.data, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    return hmac.compare_digest(expected_signature, signature_header)
