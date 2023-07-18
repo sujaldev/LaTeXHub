069850640c38982b2d598f436f3790e9f71577f3
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-18 14:22:34 +0530

    fix bool comparison bug in verify_signature

--- src/auth.py
+++ src/auth.py
@@ -18,7 +18,7 @@ def verify_signature(request: flask.Request) -> bool:
     # As described here:
     # https://docs.github.com/en/webhooks-and-events/webhooks/securing-your-webhooks#python-example
 
-    if signature_header := request.headers.get("X-Hub-Signature-256") is None:
+    if (signature_header := request.headers.get("X-Hub-Signature-256")) is None:
         return False
 
     hash_object = hmac.new(SECRET.encode('utf-8'), msg=request.data, digestmod=hashlib.sha256)
